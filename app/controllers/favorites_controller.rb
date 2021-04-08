ed75204888b72af4756f0151299f9f3132ce2318
keiadachi <2013edcckeiadachi@gmail.com>
2021-04-08 14:32:53 +0000

    iine

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,15 +3,13 @@ class FavoritesController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite = current_user.favorites.new(book_id: params[:book_id])
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite = current_user.favorites.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
